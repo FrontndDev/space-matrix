7712eaed39781efd41abf7d8dd863dfe137904e7
Egor <egor.babashko2005@mail.ru>
2024-01-08 21:57:38 +0300

    path fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const routes = [
         path: baseUrl,
         children: [
             {
-                path: '/:type?',
+                path: ':type?',
                 component: Home,
             },
             // {
