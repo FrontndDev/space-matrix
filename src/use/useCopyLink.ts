a21b977d44f84c7b28b1f88e5385bc411275a9ad
IvanVaskovich <vaskov.vany1@gmail.com>
2024-01-13 18:17:28 +0100

    fix copy

--- src/use/useCopyLink.ts
+++ src/use/useCopyLink.ts
@@ -1,7 +1,8 @@
 import { useShowMessage } from "./useShowMessage.ts";
+import { baseUrl } from "../router";
 
-export function useCopyLink(matrixId: number) {
-    const link = window.location.origin + window.location.pathname
+export function useCopyLink(matrixId: number, cell: any) {
+    const link = window.location.origin + baseUrl + '/' + cell.type
     const id = `?id=${matrixId}`
     navigator.clipboard.writeText(link + id)
     useShowMessage('green', 'Скопировано')
