896424cd4826a0d2c12ff1e3d8e9b66acd204db0
Egor <egor.babashko2005@mail.ru>
2024-01-18 01:51:40 +0300

    fix

--- src/components/Modals/ModalsPartners/MMatrixPartner/MMatrixPartner.vue
+++ src/components/Modals/ModalsPartners/MMatrixPartner/MMatrixPartner.vue
@@ -189,13 +189,7 @@ const getTypeForFirstCeil: ComputedRef<string> = computed(() => {
     return 'loading'
   }
 
-  if (!firstCeil.value?.matrix) {
-    if (!firstCeil.value?.allowBuyClone && !firstCeil.value?.allowSniper) {
-      return 'disable3'
-    }
-  }
-
-  if (firstCeil.value.matrix?.is_booster) {
+  if (firstCeil.value?.matrix?.is_booster) {
     return 'boost'
   }
 
@@ -219,13 +213,11 @@ const getTypeForSecondCeil: ComputedRef<string> = computed(() => {
     return 'loading'
   }
 
-  if (!secondCeil.value?.matrix) {
-    if (!secondCeil.value?.allowBuyClone && !secondCeil.value?.allowSniper) {
-      return 'disable3'
-    }
+  if (!secondCeil.value?.matrix && !firstCeil.value?.matrix) {
+    return 'disable'
   }
 
-  if (secondCeil.value.matrix?.is_booster) {
+  if (secondCeil.value?.matrix?.is_booster) {
     return 'boost'
   }
 
