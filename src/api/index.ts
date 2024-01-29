014932cc339e992a147675d8f63b3c5a7f2cb143
IvanVaskovich <vaskov.vany1@gmail.com>
2024-01-29 20:04:08 +0100

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,79 +1,79 @@
-import {
-  getAsync,
-  postAsync,
-  putAsync
-} from "./config.ts";
-import {
-  IExposePartnerParams
-} from "../interfaces/partners.interface.ts";
+import { getAsync, postAsync, putAsync } from "./config.ts";
+import { IExposePartnerParams } from "../interfaces/partners.interface.ts";
 import { IBuyBoosterParams } from "../interfaces/store.interface.ts";
 import { ITeleportPartnerParams } from "../interfaces/chains.interface.ts";
 
-
 export function setDataToLS(key: string, data: any) {
-  localStorage.setItem(key, JSON.stringify(data))
+  localStorage.setItem(key, JSON.stringify(data));
 }
 
 export function getDataFromLS(key: string) {
-  const data: string | null = localStorage.getItem(key)
+  const data: string | null = localStorage.getItem(key);
 
-  return data && data !== 'undefined' ? JSON.parse(data) : null
+  return data && data !== "undefined" ? JSON.parse(data) : null;
 }
 
 // GET
 
 export async function getListOfTypes(category: string) {
-  return getAsync(`/api/matrix/list-of-types/${category}`)
+  return getAsync(`/api/matrix/list-of-types/${category}`);
 }
 
-export async function getMatrix(matrixTypeOrId: string | number, options?: any) {
-  return getAsync(`/api/matrix/${matrixTypeOrId}`, options)
+export async function getMatrix(
+  matrixTypeOrId: string | number,
+  options?: any
+) {
+  return getAsync(`/api/matrix/${matrixTypeOrId}`, options);
 }
 
 export async function getPaymentForm(matrixType: string) {
-  return getAsync(`/api/matrix/payment-form/${matrixType}`)
+  return getAsync(`/api/matrix/payment-form/${matrixType}`);
 }
 
 export async function getListOfInfinity(parentId: number) {
-  return getAsync(`/api/matrix/list-of-infinity/${parentId}`)
+  return getAsync(`/api/matrix/list-of-infinity/${parentId}`);
 }
 
 export async function getListOfChains(pageID: number) {
-  return getAsync(`/api/matrix/chains/profit?page=${pageID}`)
+  return getAsync(`/api/matrix/chains/profit?page=${pageID}`);
 }
 
 export async function getListOfPartnersForTeleport(pageID: number) {
-  return getAsync(`/api/matrix/teleport/list?page=${pageID}`)
+  return getAsync(`/api/matrix/teleport/list?page=${pageID}`);
 }
 
-export async function getChainDetail(id: number) {
-  return getAsync(`/api/matrix/chain-details/${id}`)
+export async function getChainDetail(uuid: string) {
+  return getAsync(`/api/matrix/chain-details/${uuid}`);
 }
 
 export async function getBalance() {
-  return getAsync('/api/wallet/balance')
+  return getAsync("/api/wallet/balance");
 }
 
 // POST
 
-export async function filterOfActivatedMatrix({ matrixType, matrixFilterUserId, matrixFilterPageId, filter }: any) {
-  return postAsync(`/api/matrix/get-clones/${matrixType}/${matrixFilterUserId}/${matrixFilterPageId}`, { filter })
+export async function filterOfActivatedMatrix({
+  matrixType,
+  matrixFilterUserId,
+  matrixFilterPageId,
+  filter,
+}: any) {
+  return postAsync(
+    `/api/matrix/get-clones/${matrixType}/${matrixFilterUserId}/${matrixFilterPageId}`,
+    { filter }
+  );
 }
 
 export async function buyClone(data: IBuyBoosterParams) {
-  return postAsync('/api/matrix/placement/buyClone', data)
+  return postAsync("/api/matrix/placement/buyClone", data);
 }
 
 export async function activatePartnerTeleport(data: ITeleportPartnerParams) {
-  return postAsync('/api/matrix/teleport/activate', data)
+  return postAsync("/api/matrix/teleport/activate", data);
 }
 
 // PUT
 
 export async function placementExistMatrix(data: IExposePartnerParams) {
-  return putAsync('/api/matrix/placement/exist', data)
+  return putAsync("/api/matrix/placement/exist", data);
 }
-
-
-
-
--- src/store/modules/chains.ts
+++ src/store/modules/chains.ts
@@ -1,14 +1,11 @@
-import * as API from '../../api/index'
-import {
-  ActionContext,
-  Commit
-} from "vuex";
+import * as API from "../../api/index";
+import { ActionContext, Commit } from "vuex";
 import {
   IChainDetails,
   IChainsList,
   ITeleportList,
   ITeleportPartnerParams,
-  ITeleports
+  ITeleports,
 } from "../../interfaces/chains.interface.ts";
 import { useShowMessage } from "../../composables/useShowMessage.ts";
 
@@ -19,58 +16,54 @@ export default {
       chainsList: {} as IChainsList,
       teleportList: {} as ITeleportList,
       chainDetails: {} as IChainDetails,
-      pageIdChains: 1
-    }
+      pageIdChains: 1,
+    };
   },
   actions: {
-    getChainsList(
-      { commit, state }: { commit: Commit; state: any },
-    ) {
-      API.getListOfChains(state.pageIdChains).then(response => {
-        commit('SET_CHAINS_LIST', response.data)
-      })
+    getChainsList({ commit, state }: { commit: Commit; state: any }) {
+      API.getListOfChains(state.pageIdChains).then((response) => {
+        commit("SET_CHAINS_LIST", response.data);
+      });
     },
-    getTeleportList(
-        { commit, state }: { commit: Commit; state: any }
-    ) {
-      API.getListOfPartnersForTeleport(state.pageIdChains).then(response => {
-        commit('SET_TELEPORT_LIST', response.data)
-      })
+    getTeleportList({ commit, state }: { commit: Commit; state: any }) {
+      API.getListOfPartnersForTeleport(state.pageIdChains).then((response) => {
+        commit("SET_TELEPORT_LIST", response.data);
+      });
     },
-    getChainDetail(
-      { commit }: { commit: Commit; },
-      id: number
-    ) {
-      API.getChainDetail(id).then(response => {
-        commit('SET_CHAIN_DETAIL', response.data)
-      })
+    getChainDetail({ commit }: { commit: Commit }, id: string) {
+      API.getChainDetail(id).then((response) => {
+        commit("SET_CHAIN_DETAIL", response.data);
+      });
     },
-    teleportPartner({ state }: ActionContext<any, any>, data: ITeleportPartnerParams) {
-      API.activatePartnerTeleport(data).then(response => {
-        console.log('teleportPartner', response)
+    teleportPartner(
+      { state }: ActionContext<any, any>,
+      data: ITeleportPartnerParams
+    ) {
+      API.activatePartnerTeleport(data).then((response) => {
+        console.log("teleportPartner", response);
         if (response?.error_code !== undefined) {
-          useShowMessage('green', 'Телепорт успешно активирован')
+          useShowMessage("green", "Телепорт успешно активирован");
           const index = state.teleportList.list
-              .map((chain: ITeleports) => chain.owner.id)
-              .indexOf(data.partnerId)
-          state.teleportList.list.splice(index, 1)
-          state.teleportList.totalCount--
+            .map((chain: ITeleports) => chain.owner.id)
+            .indexOf(data.partnerId);
+          state.teleportList.list.splice(index, 1);
+          state.teleportList.totalCount--;
         }
-      })
-    }
+      });
+    },
   },
   mutations: {
     SET_CHAINS_LIST(state: any, chainsList: any) {
-      state.chainsList = chainsList
+      state.chainsList = chainsList;
     },
     SET_TELEPORT_LIST(state: any, teleportList: any) {
-      state.teleportList = teleportList
+      state.teleportList = teleportList;
     },
     SET_CHAIN_DETAIL(state: any, chainDetails: any) {
-      state.chainDetails = chainDetails
+      state.chainDetails = chainDetails;
     },
     SET_PAGE_ID_CHAINS(state: any, pageIdChains: number) {
-      state.pageIdChains = pageIdChains
-    }
+      state.pageIdChains = pageIdChains;
+    },
   },
-}
+};
